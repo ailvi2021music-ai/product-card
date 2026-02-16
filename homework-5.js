cbedbc561a55db8813cc191e1b3cc856fbcecd0d
Victor <ailvi2021@gmail.com>
2026-02-16 13:52:42 +0300

    fix errors

--- homework-5.js
+++ homework-5.js
@@ -1,44 +1,44 @@
 function showWeather(city, temperature) {
-  console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`)
+  console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`);
 }
 
-showWeather("Москва", +10)
+showWeather("Москва", +10);
 
 const SPEED_OF_LIGHT = 299792458;
 
 function checkSpeed(speed) {
   if (speed > SPEED_OF_LIGHT) {
-    console.log("Сверхсветовая скорость")
+    console.log("Сверхсветовая скорость");
   } else if (speed < SPEED_OF_LIGHT) {
-    console.log("Субсветовая скорость")
+    console.log("Субсветовая скорость");
   } else {
-    console.log("Скорость света")
+    console.log("Скорость света");
   }
 }
 
-checkSpeed(102)
+checkSpeed(102);
 
 const product = "MacPro";
 const price = 1000;
 
-function checkBudget(budget) { 
+function getProduct(budget) { 
   if (budget >= price) { 
-  console.log(`${product} приобретён. Спасибо за покупку!`)
+  console.log(`${product} приобретён. Спасибо за покупку!`);
   } else {
-    const difference = price - budget
-    console.log(`Вам не хватает ${difference}$, пополните баланс`)
+    const difference = price - budget;
+    console.log(`Вам не хватает ${difference}$, пополните баланс`);
   }
 }
 
-checkBudget(730)
-checkBudget(1000)
+getProduct(730);
+getProduct(1000);
 
 function greet(name) {
-  console.log(`Здравствуйте, ${name}! Добро пожаловать в Интернет-Магазин`)
+  console.log(`Здравствуйте, ${name}! Добро пожаловать в Интернет-Магазин`);
 }
 
-greet("Виктор")
+greet("Виктор");
 
 const products = ["MacPro", "IPad", "IPhone"];
 const userName = "Виктор";
-const address = "Москва, ул. Гагарина 13, кв 55"
+const address = "Москва, ул. Гагарина 13, кв 55";
